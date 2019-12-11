58b5c2b2560ea5fbd57c0b7c51572f24d9a68c85
zmeriksen <zmeriksen@gmail.com>
2019-12-10 21:18:16 -0600

    Updated tests

--- Tests/SLogTests/SLogTests.swift
+++ Tests/SLogTests/SLogTests.swift
@@ -3,11 +3,49 @@ import XCTest
 
 final class SLogTests: XCTestCase {
     func testExample() {
-        let log = SLog(level: .error, title: "error", fileName: "error", shouldLogToConsole: true)
+        let flog = SLog.fatal(title: "Fatal Logs Test", fileName: "slog.fatallogs", shouldLogToConsole: true)
+        let elog = SLog.error(title: "Error Logs To Test SLog If IT WORKS!", fileName: "slog.errorlogs", shouldLogToConsole: true)
+        let wlog = SLog.warning(title: "Warning Log", fileName: "slog.warninglogs", shouldLogToConsole: true)
+        let slog = SLog.success(title: "Success!", fileName: "slog.successlogs", shouldLogToConsole: true)
+        let ilog = SLog.info(title: "Info Logs...", fileName: "slog.infologs", shouldLogToConsole: true)
+        let dlog = SLog.debug(title: "Debug Logs!", fileName: "slog.debuglogs", shouldLogToConsole: true)
         
-        log.entry {
-            "asdfasldkjfhalsk"
+        flog.entry {
+            "Could not unwrap value!"
         }
+        elog.entry {
+            [
+            "fasdhjy6ui",
+            "697895rjmgvhjbn"
+            ]
+        }
+        wlog.entry {
+            "Don't do this... It is depricated"
+        }
+        
+        slog.entry {
+            "Correct Value... Logged in!"
+        }
+        ilog.entry {
+            ("Values:",
+            45234,
+            3467,
+            976,
+            423,
+            "fgsdf",
+            true,
+            CGSize(width: 534, height: 768))
+        }
+        dlog.entry {
+            "This is only for debug!"
+        }
+        dlog.entry(
+            "Value For the First Cell",
+            "v1",
+            324,
+            true,
+            CGColor.black
+        )
     }
 
     static var allTests = [
